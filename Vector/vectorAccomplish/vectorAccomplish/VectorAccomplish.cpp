f29a07fdf19219a797aede5e071a8b089aa080b4
1422625282@qq.com <1422625282@qq.com>
2019-10-13 22:50:14 +0800

    Add interface

--- Vector/vectorAccomplish/vectorAccomplish/VectorAccomplish.cpp
+++ Vector/vectorAccomplish/vectorAccomplish/VectorAccomplish.cpp
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
+#include <assert.h>
 using namespace std;
 
 namespace MyVector{
@@ -15,6 +16,9 @@ namespace MyVector{
 		{
 		}
 		vector(int n, const T &value = 0)
+			:_start(nullptr)
+			, _finish(nullptr)
+			, _endOfStorage(nullptr)
 		{
 			_start = new T[n+1];
 			int i = n;
@@ -28,6 +32,9 @@ namespace MyVector{
 		}
 		template <class InputIterator>
 		vector(InputIterator first, InputIterator last)    
+			:_start(nullptr)
+			, _finish(nullptr)
+			, _endOfStorage(nullptr)
 		{
 			int n=0;
 			auto tmp = first;
@@ -50,6 +57,9 @@ namespace MyVector{
 			
 		}
 		vector(const vector<T>& v)
+			:_start(nullptr)
+			, _finish (nullptr)
+			,_endOfStorage(nullptr)
 		{
 			reserve(v.capacity());
 			//memcpy(_start, v._start,v.size());
@@ -132,12 +142,15 @@ namespace MyVector{
 			}
 			_finish = _start + newsize;
 		}
+
 		//元素访问
+
+
 
 		//元素修改
 		void push_pack(T value)
 		{
-			if (_finish == _endOfStorage)
+			/*if (_finish == _endOfStorage)
 			{
 				int n = (capacity() == 0 ? 1 : (int)capacity()*1.5);
 				reserve(n);
@@ -145,16 +158,68 @@ namespace MyVector{
 			}
 
 			*_finish = value;
-			_finish++;
+			_finish++;*/
+			insert(end(), value);
 		}
 		void pop_back()
 		{
-			if (_start == _finish)
+			erase(end()-1);
+		}
+
+		iterator insert(iterator pos, const T &value)
+		{
+			assert(pos <= _finish);
+			assert(pos >=_start);
+			int n = pos - _start;
+			if (_finish == _endOfStorage)
 			{
-				return;
+				reserve(capacity() + 1);
+			}
+			pos = _start + n;
+			auto it = _finish;
+			while (it != pos)
+			{
+				*it = *(it-1);
+				it--;
+			}
+			*pos = value;
+			_finish++;
+			return pos;
+		}
+
+		iterator erase(iterator pos)
+		{
+			auto it = pos+1;
+			while (it != end())
+			{
+				*(it-1) = *it;
+				it++;
 			}
 			_finish--;
+			return pos;
 		}
+		//using namespace std;
+		void _swap(vector<T> &v)     //?????????????????????????????????????????
+		{
+			/*iterator tmp;
+			tmp = _start;
+			_start = v._start;
+			_finish = tmp;*/
+
+			swap(_start, v._start);
+			swap(_finish, v._finish);
+			swap(_endOfStorage, v._endOfStorage);
+
+		}
+
+
+		vector<T>& operator=(vector<T> v)
+		{
+			_swap(v);
+			return *this;
+		}
+
+
 		void Print()
 		{
 			iterator e = begin();
@@ -168,25 +233,43 @@ namespace MyVector{
 			cout << "capacity(): " << capacity() << endl;
 		}
 
-		friend ostream operator<<(ostream _cout, const vector<T> &v);
+		friend ostream& operator<<(const ostream &_cout, const vector<T> &v);
+		friend iterator find(iterator begin, iterator end, const T &value);
 	private:
 		T *_start;
 		T *_finish;
 		T *_endOfStorage;
 
 	};
+	
+	
+
+	}
 	template <class T>
-	ostream operator<<(ostream _cout, const vector<T> &v)
+	T* find(T* begin, T* end, const T &value)
+	{
+		auto it = begin;
+		while (it != end)
+		{
+			if (*it == value)
+			{
+				return it;
+			}
+			it++;
+		}
+		return nullptr;
+	}
+
+
+
+	/*template <class T>
+	ostream& operator<<(ostream _cout, const MyVector::vector<int> &v)
 	{
 		for (auto &e : v)
 		{
 			_cout << e;
 		}
-	}
-
-
-
-}
+	}*/
 
 
 
@@ -201,14 +284,30 @@ void Test1()
 	v4.Print();
 	v4.push_pack(2);
 	v4.Print();
+	cout << "pop_back test" << endl;
 	v4.pop_back();
 	v4.Print();
+	
+	v3.insert(find(v3.begin(), v3.end(), 2), 7);
+	v3.Print();
+	v3.erase(find(v3.begin(), v3.end(), 7));
+	v3.Print();
+	v3._swap(v2);
+	v3.Print();
+	v3 = v2;
+	v3.Print();
+
+
+	
+	//cout << v1 << endl;
 }
 
 int main()
 {
+	
+	
 	Test1();
-
+	
 
 
 	system("pause");
