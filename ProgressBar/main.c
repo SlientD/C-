535611ec33eabd9341a30d96d625335c9120af04
SlientD <1422625282@qq.com>
2019-11-14 11:52:23 +0800

    B B A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A

---
+++ ProgressBar/main.c
@@ -0,0 +1,17 @@
+
+#include <unistd.h>     //包含sleep函数的头文件
+#include <stdio.h>
+#include <string.h>
+int main(){
+
+    char buf[1024]={0};
+    const char *s="/-\\|";    //一个用于输出旋转样式的字符串
+    int i=0;
+    for(i=0;i<=100;i++){
+        buf[i]='#';
+        printf("[%d%%][%c][%s]\r",i,s[i%4],buf); //%%  转义成一个%
+        fflush(stdout);  //刷新缓冲区
+        sleep(1);
+    }
+
+}
